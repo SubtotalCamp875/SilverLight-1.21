64aa4a92102983c525037f408be0622f9296f48b
SubtotalCamp875 <jadenzh2020@gmail.com>
2024-07-03 22:01:20 -0400

    Initial commit

---
+++ src/main/java/net/subtotalcamp875/silverlight/SilverLight.java
@@ -0,0 +1,16 @@
+package net.subtotalcamp875.silverlight;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SilverLight implements ModInitializer {
+	public static String MOD_ID = "silverlight";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/subtotalcamp875/silverlight/SilverLightClient.java
@@ -0,0 +1,10 @@
+package net.subtotalcamp875.silverlight;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SilverLightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
